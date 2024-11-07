25cb911dbf13e6f285fac5c7e6aea4faf97336bd
Oscar <oscary324@gmail.com>
2024-11-06 22:22:00 -0500

    Making telemetry actually update?!?!?!??!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LinearSlidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LinearSlidesTest.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.subsystems.LinearSlides;
 import org.firstinspires.ftc.teamcode.util.GamepadWrapper;
 
@@ -20,16 +22,23 @@ public class LinearSlidesTest extends LinearOpMode {
 
         waitForStart();
 
+        ElapsedTime elapsedTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        int count = 0;
+
         while(opModeIsActive()) {
+            count++;
+            telemetry.addData("Time", "%d ms, %0.2f u/s", elapsedTime.milliseconds(), elapsedTime.milliseconds()/(double)count/1000);
 
             if (operator.isPressed(GamepadWrapper.Buttons.DPAD_UP)) {
-                slides.moveTo(slides.getPosition()+20);
+                slides.moveTo(slides.getPosition()+20); // i think 20 is too small but i forgot to update telemetry
             }
             if (operator.isPressed(GamepadWrapper.Buttons.DPAD_DOWN)) {
                 slides.moveTo(slides.getPosition()-20);
             }
 
             telemetry.addData("Linear Slides ", "%d target %d", slides.getPosition(), slides.getTargetPosition());
+
+            telemetry.update();
         }
     }
 }
