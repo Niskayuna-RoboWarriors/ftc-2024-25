e720ac60f03547cc06966dd16bd74bac7f3e0a25
crxyz0 <59106025+crxyz0@users.noreply.github.com>
2024-12-18 16:31:24 -0500

    Add most of auton code. Needs other code to be finished first.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonOpMode.java
@@ -2,25 +2,63 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive;
 import org.firstinspires.ftc.teamcode.util.GamepadWrapper;
 
 @Autonomous(name="Autonomous", group="Robot")
-public class AutonOpMode extends LinearOpMode{
+public class AutonOpMode extends LinearOpMode {
     public enum StartingSide {NET, OBSERVATION};
-    private static StartingSide startingSide; // Initialize this somehow
+    private static StartingSide startingSide;
+    private static SharedPreferences sharedPrefs;
+
+    private ElapsedTime elapsedTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
     @Override
     public void runOpMode() throws InterruptedException {
         // initialize stuff
         MecanumDrive drive = new MecanumDrive(hardwareMap);
 
-        waitForStart();
-        // run stuff
+        initSharedPreferences();
 
+        waitForStart();
+        // Should we try to score on the baskets or the chambers?
+        while (opModeIsActive()) {
+            if (startingSide == StartingSide.NET) { // Plan: just drop a sample into the net zone
+                /* Drive forward, do we want a dedicated navigation file? This sucks and we need to test it anyways.
+                   It would be better if we had one function that could move the robot anywhere.
+                */
+                elapsedTime.reset();
+                while (elapsedTime.time() < 500) {
+                    MecanumDrive.moveIn(0, 20, 0);
+                }
+                MecanumDrive.moveIn(0, 0, 0); // This is how I assume it works.
+
+                Claw. // TODO: drop
+            } else { // Should we try bringing the samples to the oberservation zone?
+
+            }
+        }
         // do stuff
         // auton
+    }
+
+    // Get shared preferences
+    public void initSharedPreferences() {
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.hardwareMap.appContext);
+
+        startingSidePref = sharedPrefs.getString("starting_side", "ERROR");
+        telemetry.addData("Starting side", startingSidePref);
+        if (startingSidePref.equals("NET")) {
+            startingSide = StartingSide.NET;
+        } else {
+            startingSide = StartingSide.OBSERVATION;
+        }
+
+        telemetry.update();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ConfigureSharedPrefsOpMode.java
@@ -0,0 +1,129 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.Arrays;
+
+// This is the code from last year. It is used to configure the shared preferences for auton.
+@TeleOp(name="configure shared prefs op mode", group="TeleOp OpMode")
+public class ConfigureSharedPrefsOpMode extends OpMode {
+
+
+    final String[][] VALUES={//all possible values of the prefs
+            {"NET","OBSERVATION"} // Starting side
+    };
+    String [] currentValues=new String[VALUES.length];
+    final String[] PREF_NAMES ={ "Starting Side" };//human-readable names of the prefs
+    int[] currentIndexes =new int[VALUES.length];
+    int currSel=0, dispUntil;
+    SharedPreferences sharedPrefs;
+    boolean prevUP,prevDOWN,prevLEFT,prevRIGHT;
+    @Override
+    @SuppressWarnings("deprecation")
+    public void init() {
+        Arrays.fill(currentIndexes, -1);//initialize all values in currentIndexes to -13
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.hardwareMap.appContext);
+
+        //display the starting info
+        telemetry.addData("ready to set shared prefs.\npres play to start","");
+        telemetry.update();
+
+        //load in all the current values
+        currentValues[0] = sharedPrefs.getString("starting_side", "ERROR");
+        for(int i=0;i<currentValues.length;i++){
+            for(int j=0;j<VALUES[i].length;j++){
+                if(currentValues[i].equals(VALUES[i][j])){
+                    currentIndexes[i]=j;//set the index value to the corresponding index for the value of the pref
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void loop() {
+        //print the controls
+        telemetry.addData("CONTROLS game pad 1:","d-pad UP/DOWN chane Selected preference. LEFT/RIGHT change value. X save current preferences");
+
+        //display all the prefs and their current values
+        for(int i=0;i<VALUES.length;i++){
+            telemetry.addData(PREF_NAMES[i],padding(16-PREF_NAMES[i].length())+((i==currSel)?"<":" ")+currentValues[i]+((i==currSel)?">":""));
+            //                                                  ^^^ if this is the currently selected pref then surround the name in < >
+        }
+        //display saved if you saved within the last second
+        if(System.nanoTime()/1000000< dispUntil){
+            telemetry.addData("SAVED","!!!!!!!");
+        }
+
+        telemetry.update();
+
+        //save the prefs when x is pressed
+        if(gamepad1.x){
+            savePrefs();
+            telemetry.addData("SAVED","!!!!");
+            telemetry.update();
+        }
+        //move to the previous pref
+        if(gamepad1.dpad_up&&!prevUP){
+            currSel--;
+            if(currSel==-1)
+                currSel=VALUES.length-1;
+        }
+        //move to the next pref
+        if(gamepad1.dpad_down&&!prevDOWN){
+            currSel++;
+            if(currSel==VALUES.length)
+                currSel=0;
+        }
+        //cycle to the previous option for the currently selected pref
+        if(gamepad1.dpad_left&&!prevLEFT){
+            currentIndexes[currSel]--;
+            if(currentIndexes[currSel]<0){
+                currentIndexes[currSel]=VALUES[currSel].length-1;
+            }
+            currentValues[currSel]=VALUES[currSel][currentIndexes[currSel]];
+        }
+        //cycle to the next option for the current pref
+        if(gamepad1.dpad_right&&!prevRIGHT){
+            currentIndexes[currSel]++;
+            if(currentIndexes[currSel]>=VALUES[currSel].length){
+                currentIndexes[currSel]=0;
+            }
+            currentValues[currSel]=VALUES[currSel][currentIndexes[currSel]];
+        }
+        //previous button states. ensures that the selection is only moved once per button press
+        prevUP=gamepad1.dpad_up;
+        prevDOWN=gamepad1.dpad_down;
+        prevLEFT=gamepad1.dpad_left;
+        prevRIGHT=gamepad1.dpad_right;
+
+    }
+
+    /**saves the prefs to the phone
+     *note: adding or removing prefs requires manually adding or removing the save/load statements
+     */
+    void savePrefs() {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("movement_mode", currentValues[0]);
+        editor.putString("auton_mode", currentValues[1]);
+        editor.putString("starting_side", currentValues[2]);
+        editor.putString("alliance_color", currentValues[3]);
+        editor.putString("parking_position", currentValues[4]);
+        editor.apply();
+        //display saved for 1 second
+        dispUntil =(int)(System.nanoTime()/1000000)+1000;
+    }
+
+    String padding(int a){
+        StringBuilder p= new StringBuilder();
+        if(a>0)
+            for(int i=0;i<a;i++){
+                p.append(" ");
+            }
+        return p.toString();
+    }
+
+}
